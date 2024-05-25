37fac981d24a2d629c4e66d6373d191729be2284
Ben Janssen <98946596+BJanssen78@users.noreply.github.com>
2024-05-25 13:40:57 +0200

    updating styling

--- src/ui-components/Navigation.jsx
+++ src/ui-components/Navigation.jsx
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navigation = () => {
+  const [activeTab, setActiveTab] = useState("home");
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <React.Fragment>
       <nav className="site-navigation">
         <ul className="navigation-list">
-          <li className="navigation-item">
+          <li
+            className={`navigation-item ${activeTab === "home" ? "active" : ""}`}
+            onClick={() => handleTabChange("home")}
+          >
             <Link className="navigation-link" to="/">
               Home
             </Link>
           </li>
-          <li className="navigation-item">
+          <li
+            className={`navigation-item ${activeTab === "about" ? "active" : ""}`}
+            onClick={() => handleTabChange("about")}
+          >
             <Link className="navigation-link" to="/about/">
               About me
             </Link>
           </li>
-          <li className="navigation-item">
+          <li
+            className={`navigation-item ${activeTab === "projects" ? "active" : ""}`}
+            onClick={() => handleTabChange("projects")}
+          >
             <Link className="navigation-link" to="/projects/">
               projects
             </Link>
           </li>
-          <li className="navigation-item">
+          <li
+            className={`navigation-item ${activeTab === "contact" ? "active" : ""}`}
+            onClick={() => handleTabChange("contact")}
+          >
             <Link className="navigation-link" to="/contact/">
               Contact
             </Link>
